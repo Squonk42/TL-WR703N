794c481b9c01f3a101c429860bcbb11ee7d24fb3
Michel Stempin <michel.stempin@wanadoo.fr>
2012-10-04 11:48:46 +0300

    Fixed SeaDragon image size and JS variable

--- javascripts/main.js
+++ javascripts/main.js
@@ -6,10 +6,10 @@ $(function() {
     // Initialize SeaDragon Deep Zoom Top Layout
     viewer_top = new Seadragon.Viewer("top_layout");
     viewer_top.openDzi("http://cache.zoom.it/content/eOel.dzi",
-			  '<?xml version="1.0" encoding="utf-8"?><Image TileSize="254" Overlap="1" Format="png" ServerFormat="Default" xmlns="http://schemas.microsoft.com/deepzoom/2009"><Size Width="3076" Height="2038" /></Image>');
+			  '<?xml version="1.0" encoding="utf-8"?><Image TileSize="254" Overlap="1" Format="png" ServerFormat="Default" xmlns="http://schemas.microsoft.com/deepzoom/2009"><Size Width="4035" Height="2508" /></Image>');
 
     // Initialize SeaDragon Deep Zoom Bottom Layout
-    viewer_top = new Seadragon.Viewer("bottom_layout");
-    viewer_top.openDzi("http://cache.zoom.it/content/ZhCD.dzi",
-			  '<?xml version="1.0" encoding="utf-8"?><Image TileSize="254" Overlap="1" Format="jpg" ServerFormat="Default" xmlns="http://schemas.microsoft.com/deepzoom/2009"><Size Width="2363" Height="945" /></Image>');
+    viewer_bottom = new Seadragon.Viewer("bottom_layout");
+    viewer_bottom.openDzi("http://cache.zoom.it/content/ZhCD.dzi",
+			  '<?xml version="1.0" encoding="utf-8"?><Image TileSize="254" Overlap="1" Format="jpg" ServerFormat="Default" xmlns="http://schemas.microsoft.com/deepzoom/2009"><Size Width="4035" Height="2508" /></Image>');
 });
