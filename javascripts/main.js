c60d353a107e2d20dc61f7b33d2ed98463ac1d63
Michel Stempin <michel.stempin@wanadoo.fr>
2012-10-04 11:06:54 +0300

    Added top & bottom layout

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,15 @@
-console.log('This would be the main JS file.');
+var view_top = null;
+var view_bottom = null;
+
+$(function() {
+
+    // Initialize SeaDragon Deep Zoom Top Layout
+    viewer_top = new Seadragon.Viewer("top_layout");
+    viewer_layout.openDzi("http://cache.zoom.it/content/eOel.dzi",
+			  '<?xml version="1.0" encoding="utf-8"?><Image TileSize="254" Overlap="1" Format="png" ServerFormat="Default" xmlns="http://schemas.microsoft.com/deepzoom/2009"><Size Width="3076" Height="2038" /></Image>');
+
+    // Initialize SeaDragon Deep Zoom Bottom Layout
+    viewer_top = new Seadragon.Viewer("bottom_layout");
+    viewer_top.openDzi("http://cache.zoom.it/content/ZhCD.dzi",
+			  '<?xml version="1.0" encoding="utf-8"?><Image TileSize="254" Overlap="1" Format="jpg" ServerFormat="Default" xmlns="http://schemas.microsoft.com/deepzoom/2009"><Size Width="2363" Height="945" /></Image>');
+});
