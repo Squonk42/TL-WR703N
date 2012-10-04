ed3ce1c48f9ee214baadd6eb5f6dfea990110374
Michel Stempin <michel.stempin@wanadoo.fr>
2012-10-04 12:06:17 +0300

    Changed format of bottom layout to PNG

--- javascripts/main.js
+++ javascripts/main.js
@@ -11,5 +11,5 @@ $(function() {
     // Initialize SeaDragon Deep Zoom Bottom Layout
     viewer_bottom = new Seadragon.Viewer("bottom_layout");
     viewer_bottom.openDzi("http://cache.zoom.it/content/ZhCD.dzi",
-			  '<?xml version="1.0" encoding="utf-8"?><Image TileSize="254" Overlap="1" Format="jpg" ServerFormat="Default" xmlns="http://schemas.microsoft.com/deepzoom/2009"><Size Width="4035" Height="2508" /></Image>');
+			  '<?xml version="1.0" encoding="utf-8"?><Image TileSize="254" Overlap="1" Format="png" ServerFormat="Default" xmlns="http://schemas.microsoft.com/deepzoom/2009"><Size Width="4035" Height="2508" /></Image>');
 });
