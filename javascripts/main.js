6c16b62fbee76baae7dcd30ae4a9e4860067dcb6
Michel Stempin <michel.stempin@wanadoo.fr>
2012-10-14 23:49:31 +0200

    Updated SeaDragon images to latest PCB

--- javascripts/main.js
+++ javascripts/main.js
@@ -6,16 +6,16 @@ $(function() {
 
     // Initialize SeaDragon Deep Zoom Top Layout
     viewer_schematic = new Seadragon.Viewer("schematic");
-    viewer_schematic.openDzi("http://cache.zoom.it/content/prPg.dzi",
+    viewer_schematic.openDzi("http://cache.zoom.it/content/xRcz.dzi",
 			  '<?xml version="1.0" encoding="utf-8"?><Image TileSize="254" TileOverlap="1" Format="png" ServerFormat="Default" xmlns="http://schemas.microsoft.com/deepzoom/2009"><Size Width="3508" Height="2479" /></Image>');
 
     // Initialize SeaDragon Deep Zoom Top Layout
     viewer_top = new Seadragon.Viewer("top_layout");
-    viewer_top.openDzi("http://cache.zoom.it/content/vuE5.dzi",
+    viewer_top.openDzi("http://cache.zoom.it/content/Nl2j.dzi",
 			  '<?xml version="1.0" encoding="utf-8"?><Image TileSize="254" Overlap="1" Format="png" ServerFormat="Default" xmlns="http://schemas.microsoft.com/deepzoom/2009"><Size Width="4156" Height="2539" /></Image>');
 
     // Initialize SeaDragon Deep Zoom Bottom Layout
     viewer_bottom = new Seadragon.Viewer("bottom_layout");
-    viewer_bottom.openDzi("http://cache.zoom.it/content/kD0N.dzi",
+    viewer_bottom.openDzi("http://cache.zoom.it/content/tnIX.dzi",
 			  '<?xml version="1.0" encoding="utf-8"?><Image TileSize="254" Overlap="1" Format="png" ServerFormat="Default" xmlns="http://schemas.microsoft.com/deepzoom/2009"><Size Width="4156" Height="2539" /></Image>');
 });
